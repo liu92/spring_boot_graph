25ca7cfbde2f8d0cc23b5aad599418aadc14cf46
liu9209 <470437289@qq.com>
2019-05-27 23:48:41 +0800

    update

--- src/main/java/com/docker/demo/SpringBootDockerApplication.java
+++ src/main/java/com/docker/demo/SpringBootDockerApplication.java
@@ -3,6 +3,9 @@ package com.docker.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lin
+ */
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
